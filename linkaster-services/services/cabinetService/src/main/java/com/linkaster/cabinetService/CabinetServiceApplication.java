4373240dd901227c889ede8a4a747a0f321157b1
javsort <javierortega336@gmail.com>
2024-11-05 19:49:43 +0100

    Dependency test. Added generalized dependencies for the entire project, and tested availability with Cabinet Service, will update the other pom.xml files for the rest of the services as well. So far good results

--- linkaster-services/services/cabinetService/src/main/java/com/linkaster/cabinetService/CabinetServiceApplication.java
+++ linkaster-services/services/cabinetService/src/main/java/com/linkaster/cabinetService/CabinetServiceApplication.java
@@ -1,18 +1,12 @@
 package com.linkaster.cabinetService;
 
-/**
- * Hello world!
- *
- */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CabinetServiceApplication
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetServiceApplication.class, args);
     }
 }
--- linkaster-services/services/cabinetService/src/main/java/com/linkaster/cabinetService/repository/FileRepository.java
+++ linkaster-services/services/cabinetService/src/main/java/com/linkaster/cabinetService/repository/FileRepository.java
@@ -1,6 +1,5 @@
 package com.linkaster.cabinetService.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface FileRepository {
 }
