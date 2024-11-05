9af695e6abbf7ee3d7613c681d80a136c340da55
javsort <javierortega336@gmail.com>
2024-11-05 20:26:51 +0100

    Successfully added the pom dependencies, basic class files, and ensured maven recognition for the ModuleManagerService

---
+++ linkaster-services/services/moduleManager/src/main/java/com/linkaster/moduleManager/ModuleManagerApplication.java
@@ -0,0 +1,10 @@
+package com.linkaster.moduleManager;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleManagerApplication.class, args);
+    }
+}
