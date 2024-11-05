5989289a34a4f8eab99641fe2cd88b017b77fbac
javsort <javierortega336@gmail.com>
2024-11-05 19:55:13 +0100

    Successfully added the pom dependencies and ensured maven recognition for the userService

---
+++ linkaster-services/services/userService/src/test/java/com/linkaster/userService/UserServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.linkaster.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
