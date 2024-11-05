1fbcfe72d909d7ae728f47ce260b8de96f2d826f
javsort <javierortega336@gmail.com>
2024-11-05 20:13:14 +0100

    Successfully added the pom dependencies, basic class files, and ensured maven recognition for the timetableService

---
+++ linkaster-services/services/timetableService/src/test/java/com/linkaster/timetableService/TimeTableServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.linkaster.timetableService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TimeTableServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
