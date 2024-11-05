0e82849925fca4d628ec15f11c7c0dc245bce504
javsort <javierortega336@gmail.com>
2024-11-05 20:22:03 +0100

    Successfully added the pom dependencies, basic class files, and ensured maven recognition for the notifHandler

---
+++ linkaster-services/services/notifHandler/src/test/java/com/linkaster/notifHandler/NotifHandlerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.linkaster.notifHandler;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class NotifHandlerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
