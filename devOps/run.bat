487fb22904fcbbe7609b03febb69460d592d98ff
javsort <javierortega336@gmail.com>
2024-11-12 21:52:02 +0100

    Properly accomodated .bat files

--- devOps/run.bat
+++ devOps/run.bat
@@ -1,45 +1,58 @@
 @echo off
+
+setlocal
+set MVN_COMMAND=mvnw.cmd
+
 if "%1"=="" (
-    echo Usage: run.bat [command]
-    echo Commands:
-    echo    build      - Clean and build the project
-    echo    buildWlogs - Clean and build the project with full logs
+    echo Usage: "./run.bat [command]"
+    echo Use this script for [command] the project!
+    echo Available commands:
+    echo    build      - Clean install and build the project
+    echo    buildWlogs - Clean install and build with full logging
     echo    package    - Package the project
     echo    validate   - Validate the project
     echo    test       - Run all tests
     echo    clean      - Clean the project
-    echo    install    - Install all modules locally - clean install
     echo    help       - Show usage
     exit /b
 )
 
 if "%1"=="build" (
+    cd ../linkaster-services
     call mvnw.cmd clean install
+
 ) else if "%1"=="buildWlogs" (
+    cd ../linkaster-services
     call mvnw.cmd clean install -X
+
 ) else if "%1"=="package" (
+    cd ../linkaster-services
     call mvnw.cmd clean package
+
 ) else if "%1"=="validate" (
+    cd ../linkaster-services
     call mvnw.cmd validate
+
 ) else if "%1"=="test" (
+    cd ../linkaster-services
     call mvnw.cmd test
+
 ) else if "%1"=="clean" (
+    cd ../linkaster-services
     call mvnw.cmd clean
-) else if "%1"=="install" (
-    call mvnw.cmd clean install
+
 ) else if "%1"=="help" (
-    echo Usage: run.bat [command]
-    echo Commands:
-    echo    build      - Clean and build the project
-    echo    buildWlogs - Clean and build the project with full logs
+    echo Usage: "./run.bat [command]"
+    echo Use this script for [command] the project!
+    echo    build      - Clean install and build the project
+    echo    buildWlogs - Clean install and build with full logging
     echo    package    - Package the project
     echo    validate   - Validate the project
     echo    test       - Run all tests
-    echo    validate   - Validate the project
     echo    clean      - Clean the project
-    echo    install    - Install all modules locally - clean install
     echo    help       - Show usage
 ) else (
     echo Invalid command
-    echo Type "run.bat help" for usage.
+    echo Type "./run.bat help" for usage.
+
 )
