e4fbc757430cbb1f054148d2e659eff9ce0e2d06
javsort <javierortega336@gmail.com>
2024-11-15 13:34:09 +0100

    commented package_n_contain

--- devOps/package_n_contain.bat
+++ devOps/package_n_contain.bat
@@ -2,10 +2,11 @@
 
 setlocal
 
-set MVN_COMMAND=mvnw.cmd
-
+:: Drop all containers and networks
 call docker-compose down
 
+:: Build the services again with /.run.bat package
 call ./run.bat package
 
+:: Build the docker images again and run the containers
 call docker-compose up --build
