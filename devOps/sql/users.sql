e1d174feaff567587162d59bf4eadc5bf7b3c2e4
javsort <javierortega336@gmail.com>
2024-11-16 21:09:10 +0100

    Tokenization is operational so far with the sample users added to the database files. Further testing will be done with method access and others

--- devOps/sql/users.sql
+++ devOps/sql/users.sql
@@ -5,12 +5,12 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     role_id BIGINT,
-    keyPair BLOB,
+    key_pair BLOB,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
 
 -- Insert an admin user
-INSERT INTO users (username, password, email, role_id, keyPair)
+INSERT INTO users (username, password, email, role_id, key_pair)
 VALUES ('admin', 'admin_password', 'admin@example.com', 1, NULL), 
        ('student', 'student_password', 'student@example.com', 2, NULL),
        ('teacher', 'teacher_password', 'teacher@example.com', 3, NULL),
