b4be02afcb8f3fce32f70dca0f4576661224f242
javsort <javierortega336@gmail.com>
2024-11-28 12:43:44 +0100

    Updated sql functionality. Needed # order for uploads. Also adding a great push from module

--- devOps/sql/003_modules.sql
+++ devOps/sql/003_modules.sql
@@ -1,19 +1,19 @@
 -- Create the users table
 CREATE TABLE modules (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    module_owner_id BIGINT NOT NULL,
+    module_owner_id BIGINT,
     module_name VARCHAR(255) NOT NULL,
-    module_code INTEGER  NOT NULL UNIQUE,
+    module_code VARCHAR(255) UNIQUE,
     student_enrolled_ids JSON,
     teacher_enrolled_ids JSON,
     events JSON,
     module_type VARCHAR(255),
-    FOREIGN KEY (module_owner_id) REFERENCES users(teacher_user_id) ON DELETE SET NULL
+    FOREIGN KEY (module_owner_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
---Instert a module class and a module club
+-- Instert a module class and a module club
 INSERT INTO modules (module_owner_id, module_name, module_code, student_enrolled_ids, teacher_enrolled_ids, events, module_type)
-VALUES (3, 'Internet Application of Engineering', 123456, NULL, NULL, NULL, 'class'),
+VALUES (3, 'Internet Application of Engineering', "123456", NULL, NULL, NULL, 'class'),
        (3, 'Robotics', 123457, NULL, NULL, NULL, 'club');
 
 
@@ -21,7 +21,6 @@ VALUES (3, 'Internet Application of Engineering', 123456, NULL, NULL, NULL, 'cla
 CREATE TABLE classes (
     id BIGINT AUTO_INCREMENT PRIMARY KEY, -- This matches the "id" in the modules table
     class_name VARCHAR(255) NOT NULL,
-    class_code TEXT NOT NULL UNIQUE,
     class_owner_id BIGINT NOT NULL,
     class_teacher_ids JSON,
     class_student_ids JSON,
@@ -31,14 +30,13 @@ CREATE TABLE classes (
     );       
 
 -- Insert a class
-INSERT INTO classes (class_name, class_code, class_owner_id, class_teacher_ids, class_student_ids, class_events)
-VALUES ('Internet Application of Engineering', 123456, 3, NULL, NULL, NULL);
+INSERT INTO classes (class_name, class_owner_id, class_teacher_ids, class_student_ids, class_events)
+VALUES ('Internet Application of Engineering', 3, NULL, NULL, NULL);
 
 -- Crate club table
 CREATE TABLE clubs (
     id BIGINT AUTO_INCREMENT PRIMARY KEY, -- This matches the "id" in the modules table
     club_name VARCHAR(255) NOT NULL,
-    club_code INTEGER NOT NULL UNIQUE,
     club_owner_id BIGINT NOT NULL,
     club_student_ids JSON,
     club_events JSON,
@@ -47,6 +45,6 @@ CREATE TABLE clubs (
     );
 
 -- Insert a club
-INSERT INTO clubs (club_name, club_code, club_owner_id, club_student_ids, club_events)
-VALUES ('Robotics', 123457, 2, NULL, NULL);
+INSERT INTO clubs (club_name, club_owner_id, club_student_ids, club_events)
+VALUES ('Robotics', 2, NULL, NULL);
 
--- devOps/sql/005_announcements.sql
+++ devOps/sql/005_announcements.sql
@@ -4,13 +4,13 @@ CREATE TABLE announcements (
     announcement_content TEXT NOT NULL,
     announcement_date DATE NOT NULL,
     announcement_time TIME NOT NULL,
-    announcement_owner_id BIGINT NOT NULL,
-    announcement_module_id BIGINT NOT NULL,
+    announcement_owner_id BIGINT,
+    announcement_module_id BIGINT,
     FOREIGN KEY (announcement_owner_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (announcement_module_id) REFERENCES modules(id) ON DELETE CASCADE
 );
 
 -- Insert data of announcements
 INSERT INTO announcements (announcement_content, announcement_date, announcement_time, announcement_owner_id, announcement_module_id)
-VALUES ('Welcome to the Internet Application of Engineering class!', '2024-12-03', '09:00:00', 3, 123456),
-       ('Welcome to the Robotics club!', '2024-12-04', '13:00:00', 2, 123457);
+VALUES ('Welcome to the Internet Application of Engineering class!', '2024-12-03', '09:00:00', 3, 1),
+       ('Welcome to the Robotics club!', '2024-12-04', '13:00:00', 2, 2);
