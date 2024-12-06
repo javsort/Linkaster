f8b88f0b3b688a85b47b8082807fffa4f72fe4d1
javsort <javierortega336@gmail.com>
2024-12-06 14:51:40 +0100

    Finished README for the project. Added a couple notes as well, and minor fixes

--- devOps/scripts/FULL_package_n_contain.sh
+++ devOps/scripts/FULL_package_n_contain.sh
@@ -2,17 +2,19 @@
 
 # WARNING!!! This script builds the ENTIRE app, takes a while to run and requires a lot of resources.
 
+# Ensure necessary scripts are executable
+chmod +x ./run.sh
+
 # Stop and remove all containers and networks
 docker-compose -f ../docker-compose-backend.yml down
 docker-compose -f ../docker-compose-full.yml down
 docker-compose -f ../docker-compose-frontonly.yml down
 
+# Build the backend with /.run.bat package
+./run.sh package
 
-:: Build the backend with /.run.bat package
-./run.bat package
-
-:: Build the docker images again and run the containers
+# Build the docker images again and run the containers
 docker-compose -f ../docker-compose-full.yml build --no-cache
 
-:: Run the containers
+# Run the containers
 docker-compose -f ../docker-compose-full.yml up
